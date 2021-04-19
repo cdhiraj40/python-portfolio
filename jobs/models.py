7b53dfba22f8431cce6f78b6d8bfc376c6876f44
Dhiraj Chauhan <chauhandhiraj40@gmail.com>
2021-04-19 18:34:19 +0530

    added one job to portfolio

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+"""it essentially it let us create a class Job and
+models.Model gives us all sort of background stuff
+that allows us to save the object here"""
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')#CAN SEARCH DJANGO MODELFIELDS
+    summary = models.CharField(max_length=200)#so that all the box looks same in size
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_URl = '/media/'
